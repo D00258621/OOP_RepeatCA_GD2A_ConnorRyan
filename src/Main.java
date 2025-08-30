44c9a371befec44a0ae1af13bee3bd1ef1f738fb
D00258621 <d00258621@student.dkit.ie>
2025-08-30 13:23:57 +0100

    DA0 Interface
    Creating DAO interface and running test in checking if parameters get filled

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 //        Player player = new Player(1, "Alice", 22, 62.4);
 //        System.out.println(player);
+//        Player p = new Player(1, "Ben", 23, 72.1);
+//        System.out.println(p);
+
     }
 }
