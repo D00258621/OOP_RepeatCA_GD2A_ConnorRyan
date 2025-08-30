c770c06e89f1447dac5030ba39adc3c347c7beac
D00258621 <d00258621@student.dkit.ie>
2025-08-30 13:08:36 +0100

    Player DTO creation
    Create player DTO class, and main. Test if DTO object worked

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+//        Player player = new Player(1, "Alice", 22, 62.4);
+//        System.out.println(player);
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,56 @@
+public class Player {
+
+    private int id;
+    private String name;
+    private int age;
+    private double winrate;
+
+    public Player(int id, String name, int age, double winrate) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.winrate = winrate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWinrate() {
+        return winrate;
+    }
+
+    public void setWinrate(double winrate) {
+        this.winrate = winrate;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", winrate=" + winrate +
+                '}';
+    }
+}
