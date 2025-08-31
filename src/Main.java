58be77621011e0ada7646133c845d330abee9408
D00258621 <d00258621@student.dkit.ie>
2025-08-31 13:04:07 +0100

    Data Access Object for Player
    Creation of Player DAO and creating the methods to be used by Main Created the skeleton of most but only implemented one get By Id and tested in Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 //        Player player = new Player(1, "Alice", 22, 62.4);
 //        System.out.println(player);
 //        Player p = new Player(1, "Ben", 23, 72.1);
 //        System.out.println(p);
-
+        PlayerDAO playerDAO = new PlayerDAO();
+        Player player = playerDAO.get(7);
+        System.out.println(player);
+        int result = playerDAO.delete(player);
+        System.out.println(result);
     }
 }
--- src/PlayerDAO.java
+++ src/PlayerDAO.java
@@ -1,3 +1,48 @@
-public interface PlayerDAO extends DAO<Player> {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerDAO implements InterfaceDAO {
+
+    @Override
+    public List<Player> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Player get(int id) throws SQLException {
+        Connection connection = SqlConnection.getConnection();
+        Player player = null;
+        String sql = "SELECT id, name, age, winrate FROM players WHERE id = ?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            int dID = rs.getInt("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            double winRate = rs.getDouble("winrate");
+
+            player = new Player(dID, name, age, winRate);
+        }
+        SqlConnection.closeConnection(connection);
+        SqlConnection.closedPrepareStatement(ps);
+        SqlConnection.closeResultSet(rs);
+        return player;
+    }
+    @Override
+    public int insert(Player player) throws SQLException {
+      return 0;
+    }
+
+    @Override
+    public int update(Player player) {
+        return 0;
+    }
+    @Override
+    public int delete(Player player) {
+        return 0;
+    }
 
 }
